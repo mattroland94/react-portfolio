94bd169a11014de3c7e8d507fa4493a0f12d408e
Matt Roland <83994674+mattroland94@users.noreply.github.com>
2022-01-21 16:03:16 -0600

    created the project section of the application

---
+++ comps/project/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {FaGithub, FaRocket } from 'react-icons/fa';
+import projects from './projects.json';
+import './style.css';
+
+function project() {
+    return (
+        <div className="projects row justify-content-center">
+            {
+                projects && projects.length > 0 && projects.map((project) =>
+                <div className="project col-lg-5 col-md-11">
+                    <img src={require('../../assets/images/applications/' + project.image + '.png')} alt={project.title} />
+                    <div className="project-info">
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <div className="icons">
+                            <i><a href={project.githubLink}><FaGithub /></a></i>
+                            <i><a href={project.appLink}><FaRocket /></a></i>
+                        </div>
+                    </div>
+                </div>)
+            }
+        </div>
+    )
+}
+
+export default project;
