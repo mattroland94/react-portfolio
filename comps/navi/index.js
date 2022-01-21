bc18dfe4b02f8b7bcc955883948991fb86ab2258
Matt Roland <83994674+mattroland94@users.noreply.github.com>
2022-01-21 13:36:25 -0600

    Created the navigation section of the react applicaiton

---
+++ comps/navi/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './style.css';
+import 'animate.css';
+
+function navi(props) {
+    let name = '<Matt Roland />';
+
+    return (
+        <header className='flex-wrap'>
+            <h1 className='fw-bold'>
+                {name}
+            </h1>
+            <nav className='d-flex flex-wrap'>
+                <ul className='d-flex justify-content-between mb-0'>
+                    <li>
+                        <a href='#about' className='text-decoration-none' onClick={() => props.setCurrentDisplay("about")}>About</a>
+                    </li>
+                    <li>
+                        <a href='#portfolio' className='text-decoration-none' onClick={() => props.setCurrentDisplay("portfolio")}>Portfolio</a>
+                    </li>
+                    <li>
+                        <a href='#resume' className='text-decoration-none' onClick={() => props.setCurrentDisplay("resume")}>Resume</a>
+                    </li>
+                    <li>
+                        <a href='#contact' className='text-decoration-none' onClick={() => props.setCurrentDisplay("contact")}>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default navi;
