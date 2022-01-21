98f456b7be46a30eec237cbab4fdf2dabfd6a5cf
Matt Roland <83994674+mattroland94@users.noreply.github.com>
2022-01-20 19:55:50 -0600

    Made the contact index.js file with functionality and what is needed to display properly

---
+++ comps/contact/index.js
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+import {validateEmail} from '../../utils/helpers';
+import './style.css';
+
+function Contact() {
+    const [formStat, setFormStat] = useState({name: '', email: '', message: ''});
+    const [errorMessage, setErrorMessage] = useState('');
+    const {name, email, message} = formState;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+            setFormState({[e.target.name]: e.target.value});
+            console.log('Form', formStat);
+        }
+    };
+
+    const handleChange = (e) => {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Email is invalid');
+            }
+            else {
+                setErrorMessage('');
+            }
+        }
+        else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required`);
+            }
+            else {
+                setErrorMessage('');
+            }
+        }
+    };
+
+    return (
+        <div className='contact-container d-flex justify-content-center row'>
+            <h2>Contact Me</h2>
+            <section className='contact col-lg-4 col-md-8 col-sm-8'>
+                <div className='contact-form'>
+                    <form onSubmit={handleSubmit}>
+                        <div className='form-box'>
+                            <label htmlFor='name'>Name:</label>
+                            <input type='text' name='name' defaultValue={name} onBlur={handleChange} />
+                        </div>
+                        <div className='form-box'>
+                            <label htmlFor='email'>Email:</label>
+                            <input type='text' name='email' defaultValue={email} onBlur={handleChange} />
+                        </div>
+                        <div className='form-box'>
+                            <label htmlFor='message'>Message:</label>
+                            <textarea name='name' rows='5' defaultValue={message} onBlur={handleChange} />
+                        </div>
+                        {errorMessage && (
+                            <div>
+                                <p className='error-text'>{errorMessage}</p>
+                            </div>
+                        )}
+                        <button className='btn btn-success' type='submit'>Send Message</button>
+                    </form>
+                </div>
+            </section>
+            <section className='info col-lg-4 col-md-8 col-sm-8'>
+                <h3>Let me know what you think about my projects or just introudce yourself. I would love to hear from you.
+                    <br></br>
+                    Just fill out this for and send me an email.
+                    <br></br>
+                    <br></br>
+                    Thanks!
+                </h3>
+            </section>
+        </div>
+    )
+}
+
+export default Contact;
